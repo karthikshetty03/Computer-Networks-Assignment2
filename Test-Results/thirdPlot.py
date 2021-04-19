ea5472b4193d5a51d132872b86917f725949d0a0
Karthik Shetty <f20180141@hyderabad.bits-pilani.ac.in>
2021-04-19 19:53:21 +0530

    Netem Testing

---
+++ Test-Results/thirdPlot.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+file1 = open("thirdPlot.txt", "r")
+fin = file1.read()
+lines = fin.split("\n")
+
+plt.title("Throughput v/s Packet Corruption")
+plt.xlabel("Network Delay (in ms)")
+plt.ylabel("Bandwidth (B/s)")
+
+i = 0
+while i < len(lines):
+    x, y = lines[i].split(" ")
+    x = float(x)
+    y = float(y)
+    plt.scatter(x, y, color="b")
+    i += 1
+
+i = 0
+while i < len(lines):
+    if i + 1 > len(lines) - 1:
+        break
+    x1, y1 = lines[i].split(" ")
+    x2, y2 = lines[i + 1].split(" ")
+    x1 = float(x1)
+    y1 = float(y1)
+    x2 = float(x2)
+    y2 = float(y2)
+    plt.plot([x1, x2], [y1, y2], "g")
+    i += 1
+
+plt.show()
